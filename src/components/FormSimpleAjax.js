83e85fad6d468b9363bea6b4809bf076c49add10
Patrick <39915222+paddy420Smokers@users.noreply.github.com>
2019-12-05 14:36:28 +0200

    Update FormSimpleAjax.js

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -164,7 +164,7 @@ class Form extends React.Component {
           </label>
           <div
             className="g-recaptcha"
-            data-sitekey="6LfKN3kUAAAAAGIM1CbXmaRZx3LIh_W2twn1tzkA"
+            data-sitekey="6LeqQMYUAAAAABORgim2BktJ6oWWvZDteCD2Ksyo"
           />
           {!!subject && <input type="hidden" name="subject" value={subject} />}
           <input type="hidden" name="form-name" value={name} />
