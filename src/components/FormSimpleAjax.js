5ed3dc096c2a91b02e6568792a915f136d341fd2
Patrick <39915222+paddy420Smokers@users.noreply.github.com>
2019-12-08 13:44:49 +0200

    Update FormSimpleAjax.js

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -67,7 +67,7 @@ class Form extends React.Component {
           action={action}
           onSubmit={this.handleSubmit}
           data-netlify=""
-          netlify-recaptcha=""
+          data-netlify-recaptcha="true"
         >
           {this.state.alert && (
             <div className="Form--Alert">{this.state.alert}</div>
