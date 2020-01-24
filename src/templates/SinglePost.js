8fb96dae990cb069e9a512843327a5bab510197e
Patrick <39915222+paddy420Smokers@users.noreply.github.com>
2020-01-24 12:44:46 +0200

    Update SinglePost.js

--- src/templates/SinglePost.js
+++ src/templates/SinglePost.js
@@ -121,6 +121,7 @@ export const SinglePostTemplate = ({
 </div>
 </div>
 </div>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5e2927b3c07a82ea"></script>
 </div>
 </div>
 
