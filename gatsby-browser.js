781c3f85cf6cb01b4e16f04646824937c1544876
Patrick <39915222+paddy420Smokers@users.noreply.github.com>
2020-01-27 16:51:20 +0200

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,3 @@
-exports.onRouteUpdate = ({ location, prevLocation }) => {
-  console.log('new pathname', location.pathname)
-  console.log('old pathname', prevLocation ? prevLocation.pathname : null)
-}
 exports.onInitialClientRender = () => {
   console.log("ReactDOM.render has executed")
 }
