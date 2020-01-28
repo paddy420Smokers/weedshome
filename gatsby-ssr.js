30cedaf6bc53858eb99e796295cea6d32e579291
Patrick <39915222+paddy420Smokers@users.noreply.github.com>
2020-01-28 21:43:51 +0200

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,1 @@
-import React from "react";
-export function onRenderBody(
-  { setPreBodyComponents }) {
-setPreBodyComponents([
-     <script
-        type="text/javascript"
-        src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5e2927b3c07a82ea"
-      />,
-]);
-}
+
