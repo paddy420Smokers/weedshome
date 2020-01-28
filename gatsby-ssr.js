272a885bf469468d11ef5056f652d93fb1ab12c6
Patrick <39915222+paddy420Smokers@users.noreply.github.com>
2020-01-28 09:05:49 +0200

    Create gatsby-ssr.js

---
+++ gatsby-ssr.js
@@ -0,0 +1,12 @@
+const React = require("react")
+exports.onRenderBody = ({
+  setPostBodyComponents,
+}) => {
+  setPostBodyComponents([
+    <script
+      key="mantis"
+      type="text/javascript"
+      src="https://assets.mantisadnetwork.com/mantodea.min.js"
+    />,
+  ])
+}
