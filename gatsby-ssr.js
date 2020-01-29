ddd2496008cbd22cd6f70c6eb6e44d5b0372e71b
Patrick <39915222+paddy420Smokers@users.noreply.github.com>
2020-01-29 12:16:46 +0200

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,9 +3,8 @@ export function onRenderBody(
   { setPostBodyComponents }) {
 setPostBodyComponents([
 	<script
-         key="pqr"
 	  type="text/javascript"
-         src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5e2927b3c07a82ea"
+          src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5e2927b3c07a82ea"
         />,
 ]);
 
